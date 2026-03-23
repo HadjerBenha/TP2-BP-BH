ba4e49c9f96f4b7cf412e229a5ab986ae191c110
prisca-prog <2448661@cegeplimoilou.ca>
2026-03-23 14:55:30 -0400

    Modifier des erreurs

--- tests/Librairie.java
+++ tests/Librairie.java
@@ -1,7 +1,6 @@
 package tests;
 
-import livres.Auteur;
-import livres.Ouvrage;
+import livres.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,23 +10,23 @@ public class Librairie {
     private List<Ouvrage> ouvrages = new ArrayList<>();
 
     public Librairie(){
-        Auteur albertine = new Auteur("Albertine", "Tremblay", "Canada");
-        Auteur john = new Auteur("John", "Smith", "Etats-Unis");
-        Auteur jean = new Auteur("Jean", "Némarre", "Suisse");
+        Auteur albertine = new Auteur("Albertine", "Tremblay", new Pays("Canada", "CAN"));
+        Auteur john = new Auteur("John", "Smith", new Pays("Etats Unies", "USA"));
+        Auteur jean = new Auteur("Jean", "Némarre", new Pays("Suisse", "SUI"));
         auteurs.add(albertine);
         auteurs.add(john);
         auteurs.add(jean);
 
-        ouvrages.add(new Ouvrage("Titre 1", john));
-        ouvrages.add(new Ouvrage("Titre 2", albertine));
-        ouvrages.add(new Ouvrage("Titre 3", john));
-        ouvrages.add(new Ouvrage("Titre 4", john));
-        ouvrages.add(new Ouvrage("Titre 5", albertine));
-        ouvrages.add(new Ouvrage("Titre 6", john));
-        ouvrages.add(new Ouvrage("Titre 7", john));
-        ouvrages.add(new Ouvrage("Titre 8", albertine,Ouvrage.Format.VIDEO));
-        ouvrages.add(new Ouvrage("Titre 9", john,Ouvrage.Format.AUDIO));
-        ouvrages.add(new Ouvrage("Titre 10", jean));
+        ouvrages.add(new OuvragePapier("Titre 1", john,25));
+        ouvrages.add(new OuvragePapier("Titre 2", albertine, 80));
+        ouvrages.add(new OuvragePapier("Titre 3", john,45));
+        ouvrages.add(new OuvragePapier("Titre 4", john,90));
+        ouvrages.add(new OuvragePapier("Titre 5", albertine,68));
+        ouvrages.add(new OuvragePapier("Titre 6", john,74));
+        ouvrages.add(new OuvragePapier("Titre 7", john,54));
+        ouvrages.add(new OuvrageVideo("Titre 8", albertine, 20, 2.50));
+        ouvrages.add(new OuvrageAudio("Titre 9", john,20, Format.ANALOGIQUE));
+        ouvrages.add(new OuvragePapier("Titre 10", jean,59));
     }
 
     public List<Auteur> getAuteurs() {
